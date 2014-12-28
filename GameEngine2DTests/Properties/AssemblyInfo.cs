d170daaba6c78a760889f5a3bf99689b0f24006b
gmich <g.mich90@gmail.com>
2014-12-29 00:54:29 +0200

    Updated the test project

--- GameEngine2DTests/Properties/AssemblyInfo.cs
+++ GameEngine2DTests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RetroGameEngineTests")]
+[assembly: AssemblyTitle("GameEngine2DTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RetroGameEngineTests")]
+[assembly: AssemblyProduct("GameEngine2DTests")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
