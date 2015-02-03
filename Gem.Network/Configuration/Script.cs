0023d25085f51aad8726c88a4a75fbdb57197374
George Michaelides <g.mich90@gmail.com>
2015-02-03 13:37:15 +0200

    Create Script.cs

---
+++ Gem.Network/Configuration/Script.cs
@@ -0,0 +1,7 @@
+//var PrintSum = CSScript.LoadMethod(
+       // @"public static void PrintSum(int a, int b)
+          //{
+            //  Console.WriteLine((a+b));
+         // }")
+         // .GetStaticMethod();
+//PrintSum(1, 2);
