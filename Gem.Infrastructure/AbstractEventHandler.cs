15f3e90d4ec8e922d9d75d77745534223f92fe86
Gmich <g.mich90@gmail.com>
2015-04-08 02:04:11 +0300

    Add formatting to action appender and abstract event handler

---
+++ Gem.Infrastructure/AbstractEventHandler.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Gem.Infrastructure
+{
+    public abstract class AbstractEventHandler<TEventArgs>
+        where TEventArgs : EventArgs, new()
+    {
+
+        private readonly Dictionary<string, EventHandler<TEventArgs>> Handlers =
+                     new Dictionary<string, EventHandler<TEventArgs>>();
+
+        public void Add(string system, EventHandler<TEventArgs> ev)
+        {
+            if (Handlers.ContainsKey(system))
+            {
+                Handlers[system] += ev;
+            }
+            else
+            {
+                Handlers.Add(system, ev);
+            }
+        }
+
+        public void Remove(string system, EventHandler<TEventArgs> ev)
+        {
+            Handlers[system] -= ev;
+        }
+
+        public void Fire(string system, TEventArgs args)
+        {
+            if (string.IsNullOrEmpty(system))
+            {
+                EventHandler<TEventArgs> handler = null;
+                Handlers.TryGetValue(system, out handler);
+
+                if (handler != null)
+                {
+                    handler(null, args);
+                }
+
+                handler = Handlers[""] as EventHandler<TEventArgs>;
+                if (handler != null)
+                {
+                    handler(null, args);
+                }
+            }
+        }
+    }
+}
+
--- Gem.Infrastructure/Logging/ActionAppender.cs
+++ Gem.Infrastructure/Logging/ActionAppender.cs
@@ -1,9 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Gem.Infrastructure.Logging
 {
     /// <summary>
-    /// Appends information using an Action<string> method
+    /// Appends strings using an Action<string> delegate.
+    /// FormatTemplate options:
+    /// %Verbosity - The output verbosity
+    /// %Date{pattern} - The DateTime.Now %Date{0} for default
+    /// %Message - The output message
+    /// %NewLine - Environment.NewLine
     /// </summary>
     public class ActionAppender : IAppender
     {
@@ -11,13 +20,11 @@ namespace Gem.Infrastructure.Logging
         #region Readonly Fields
 
         private readonly Action<string> Echo;
+        private readonly string FormatTemplate;
 
         #endregion
 
         #region Appending Info
-
-        private const char Separator = '-';
-        private const string EchoFormat = "{0} {1} {2}";
 
         private static class Verbosity
         {
@@ -28,23 +35,84 @@ namespace Gem.Infrastructure.Logging
             internal const string Fatal = "Fatal";
         }
 
+        private string ApplyFormatTemplate(string message, string verbosity)
+        {
+            var sb = new StringBuilder();
+
+            sb.Append(Formatter.Parse(FormatTemplate));
+            sb.Replace("%Verbosity", verbosity);
+            sb.Replace("%Message", message);
+            sb.Replace("%NewLine", Environment.NewLine);
+
+            return sb.ToString();
+        }
+
+        #endregion
+
+        #region Formatting
+
+        /// <summary>
+        /// Immutable Token
+        /// </summary>
+        internal class FormattingRule
+        {
+            private readonly Regex pattern;
+            /// <summary>
+            /// Provide the new format by the pattern's match
+            /// </summary>
+            private readonly Func<string, string> replacementRule;
+
+            internal FormattingRule(Regex pattern, Func<string, string> replacementRule)
+            {
+                this.pattern = pattern;
+                this.replacementRule = replacementRule;
+            }
+
+            public string ApplyPattern(string message)
+            {
+                return pattern.Replace(message, match => replacementRule(match.Value));
+            }
+        }
+
+        /// <summary>
+        /// Applies the formatting rules
+        /// </summary>
+        internal class Formatter
+        {
+            internal static List<FormattingRule> rules
+                  = new List<FormattingRule>
+            {                
+                 new FormattingRule(pattern: new Regex("%Date{.*}"),
+                                    replacementRule: match => 
+                                    String.Format(match.Replace("%Date", ""), DateTime.Now))                                               
+            };
+
+            internal static string Parse(string msg)
+            {
+                string result = msg;
+                rules.ForEach(rule => result = rule.ApplyPattern(msg));
+
+                return result;
+            }
+        }
+
         #endregion
 
         #region Ctor
 
-        public ActionAppender(Action<string> echo)
+        public ActionAppender(Action<string> echo, string formatTemplate = "[%Date{0:G}] %Verbosity - %Message")
         {
+            if (formatTemplate == null)
+            {
+                throw new ArgumentNullException("formatTemplate");
+            }
+            FormatTemplate = formatTemplate;
             Echo = echo;
         }
 
         #endregion
 
         #region Append
-
-        public void Write(string message)
-        {
-            Echo(message);
-        }
 
         public void Info(string message)
         {
@@ -105,12 +173,28 @@ namespace Gem.Infrastructure.Logging
             return String.Format(FormatMessage(verbosity, message), args);
         }
 
-        private string FormatMessage(string severity, string message)
+        private string FormatMessage(string verbosity, string message)
         {
-            return String.Format(EchoFormat, severity, Separator, message);
+            return ApplyFormatTemplate(message, verbosity);
         }
 
         #endregion
 
+    }
+
+    internal static class ActionAppenderFormattingExtensions
+    {
+        /// <summary>
+        /// Helper extension for adding formatting rules to the appender
+        /// </summary>
+        /// <param name="appender"></param>
+        /// <param name="tokens"></param>
+        /// <returns></returns>
+        internal static ActionAppender AddFormattingRules(this ActionAppender appender,
+                                                        ActionAppender.FormattingRule[] tokens)
+        {
+            ActionAppender.Formatter.rules.AddRange(tokens);
+            return appender;
+        }
     }
 }
