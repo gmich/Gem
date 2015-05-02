68f2fab2bf49a77225224a6599b469a30618ee05
Gmich <g.mich90@gmail.com>
2015-05-02 04:01:20 +0300

    Update the AssignDefaultValues in the DefaultValueHelper

--- Gem.Infrastructure/Attributes/DefaultValueHelper.cs
+++ Gem.Infrastructure/Attributes/DefaultValueHelper.cs
@@ -23,12 +23,12 @@ namespace Gem.Infrastructure.Attributes
                                                                                        Expression<Func<TSource, TDefaultValueType>> propertyRetriever)
 
             where TSource : class
-            //Not yet tested for classes
-            //where TDefaultValueType : struct
+        //Not yet tested for classes
+        //where TDefaultValueType : struct
         {
             var propertyInfo = GetPropertyInfo(instance, propertyRetriever);
 
-            return (TDefaultValueType)GetDefaultValueFor(instance, propertyInfo); 
+            return (TDefaultValueType)GetDefaultValueFor(instance, propertyInfo);
         }
 
         /// <summary>
@@ -55,10 +55,10 @@ namespace Gem.Infrastructure.Attributes
         /// <typeparam name="TProperty">The property's type</typeparam>
         /// <typeparam name="TSource">The source</typeparam>
         /// <returns>The property info that's returned in the expression</returns>
-        private static PropertyInfo GetPropertyInfo<TProperty, TSource>(TSource source, 
+        private static PropertyInfo GetPropertyInfo<TProperty, TSource>(TSource source,
                                                                         Expression<Func<TSource, TProperty>> propertyLambda)
                where TSource : class
-              //where TProperty : struct
+        //where TProperty : struct
         {
             Type type = typeof(TSource);
 
@@ -86,12 +86,8 @@ namespace Gem.Infrastructure.Attributes
             return propInfo;
         }
 
-        /// <summary>
-        /// Assigns all the default values to the properties that are defined with the <see cref="DefaultValueAttribute"/>
-        /// </summary>
-        /// <typeparam name="TClass">The object's type</typeparam>
-        /// <param name="instance">The instance of the object to assign the default values</param>
-        public static void AssignDefaultValues<TClass>(this TClass instance)
+        [Obsolete("Use AssignDefaultValues<TClass> instead")]
+        private static void AssignDefaultValues_Depricated<TClass>(this TClass instance)
             where TClass : class
         {
             typeof(TClass)
@@ -101,8 +97,27 @@ namespace Gem.Infrastructure.Attributes
             .ForEach(property =>
                 {
                     if (property != null)
-                        property.SetValue(instance, GetDefaultValueFor(instance,property));
+                        property.SetValue(instance, GetDefaultValueFor(instance, property));
                 });
+        }
+
+        /// <summary>
+        /// Assigns all the default values to the properties that are defined with the <see cref="DefaultValueAttribute"/>
+        /// </summary>
+        /// <typeparam name="TClass">The object's type</typeparam>
+        /// <param name="instance">The instance of the object to assign the default values</param>
+        public static void AssignDefaultValues<TClass>(this TClass instance)
+        {
+            (from property in typeof(TClass).GetProperties()
+             from defaultValueAttribute in property.GetCustomAttributes(true).OfType<Attribute>()
+             where defaultValueAttribute is DefaultValueAttribute
+             select new
+             {
+                 Property = property,
+                 DefaultValueAttribute = (DefaultValueAttribute)defaultValueAttribute
+             })
+             .ToList()
+             .ForEach(x => x.Property.SetValue(instance, x.DefaultValueAttribute.Value));
         }
     }
 }
