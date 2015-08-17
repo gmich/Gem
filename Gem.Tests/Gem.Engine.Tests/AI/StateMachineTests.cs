48aa3c9b8d0b2f405c8a928b41d6d44697d14374
George Michaelides <g.mich90@gmail.com>
2015-08-18 01:24:05 +0300

    Fixed the state machine failing test. Updated projects to target .NET framework 4.6

--- Gem.Tests/Gem.Engine.Tests/AI/StateMachineTests.cs
+++ Gem.Tests/Gem.Engine.Tests/AI/StateMachineTests.cs
@@ -17,41 +17,41 @@ namespace Gem.Engine.Tests.AI
         {
             #region States
 
-            var active = new State<SmartPhoneScreen>(screen => { }).Named("Active");
-            var exit = new State<SmartPhoneScreen>(screen => { }).Named("Exit");
-            var paused = new State<SmartPhoneScreen>(screen => { }).Named("Paused");
-            var inactive = new State<SmartPhoneScreen>(screen => { }).Named("Inactive");
+            var active = new State<SmartPhoneScreenContext>(screen => { }).Named("Active");
+            var exit = new State<SmartPhoneScreenContext>(screen => { }).Named("Exit");
+            var paused = new State<SmartPhoneScreenContext>(screen => { }).Named("Paused");
+            var inactive = new State<SmartPhoneScreenContext>(screen => { }).Named("Inactive");
 
             #endregion
 
             #region Transitions
 
-            active.AddTransition(Transition<SmartPhoneScreen>
+            active.AddTransition(Transition<SmartPhoneScreenContext>
                 .To(() => paused)
                 .When(screen => screen.Command == PauseCommand)
                 .Named("Pausing"));
 
-            active.AddTransition(Transition<SmartPhoneScreen>
+            active.AddTransition(Transition<SmartPhoneScreenContext>
                 .To(() => inactive)
                 .When(screen => screen.Command == EndCommand)
                 .Named("Going inactive"));
 
-            inactive.AddTransition(Transition<SmartPhoneScreen>
+            inactive.AddTransition(Transition<SmartPhoneScreenContext>
                 .To(() => active)
                 .When(screen => screen.Command == BeginCommand)
                 .Named("Activating"));
 
-            inactive.AddTransition(Transition<SmartPhoneScreen>
+            inactive.AddTransition(Transition<SmartPhoneScreenContext>
                 .To(() => exit)
                 .When(screen => screen.Command == ExitCommand)
                 .Named("Exiting"));
 
-            paused.AddTransition(Transition<SmartPhoneScreen>
+            paused.AddTransition(Transition<SmartPhoneScreenContext>
                 .To(() => inactive)
                 .When(screen => screen.Command == EndCommand)
                 .Named("Going inactive"));
 
-            paused.AddTransition(Transition<SmartPhoneScreen>
+            paused.AddTransition(Transition<SmartPhoneScreenContext>
                 .To(() => exit)
                 .When(screen => screen.Command == ResumeCommand)
                 .Named("Activating"));
@@ -60,42 +60,42 @@ namespace Gem.Engine.Tests.AI
 
             var smartPhoneHandler = StateMachine.Create(inactive);
 
-            var context = new SmartPhoneScreen();
+            var context = new SmartPhoneScreenContext();
 
             context.Command = BeginCommand;
             smartPhoneHandler.Operate(context);
-            Assert.Equals(smartPhoneHandler.ActiveState,active);
+            Assert.AreEqual(smartPhoneHandler.ActiveState, active);
 
             context.Command = PauseCommand;
             smartPhoneHandler.Operate(context);
-            Assert.Equals(smartPhoneHandler.ActiveState, paused);
+            Assert.AreEqual(smartPhoneHandler.ActiveState, paused);
 
             smartPhoneHandler.Operate(context);
-            Assert.Equals(smartPhoneHandler.ActiveState, paused);
+            Assert.AreEqual(smartPhoneHandler.ActiveState, paused);
 
             context.Command = EndCommand;
             smartPhoneHandler.Operate(context);
-            Assert.Equals(smartPhoneHandler.ActiveState, inactive);
+            Assert.AreEqual(smartPhoneHandler.ActiveState, inactive);
 
             smartPhoneHandler.Operate(context);
-            Assert.Equals(smartPhoneHandler.ActiveState, inactive);
+            Assert.AreEqual(smartPhoneHandler.ActiveState, inactive);
 
             context.Command = BeginCommand;
             smartPhoneHandler.Operate(context);
-            Assert.Equals(smartPhoneHandler.ActiveState, active);
+            Assert.AreEqual(smartPhoneHandler.ActiveState, active);
 
             context.Command = EndCommand;
             smartPhoneHandler.Operate(context);
-            Assert.Equals(smartPhoneHandler.ActiveState, inactive);
+            Assert.AreEqual(smartPhoneHandler.ActiveState, inactive);
 
             context.Command = ExitCommand;
             smartPhoneHandler.Operate(context);
-            Assert.Equals(smartPhoneHandler.ActiveState, exit);
+            Assert.AreEqual(smartPhoneHandler.ActiveState, exit);
         }
 
         #region Context
 
-        public class SmartPhoneScreen
+        public class SmartPhoneScreenContext
         {
             public string Command { get; set; } = string.Empty;
         }
